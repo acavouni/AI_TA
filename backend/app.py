63a536dc7b28d23fe3cac935e612e470da99bd6c
Paul Cavounis <paulcavounis@pal-nat186-11-239.itap.purdue.edu>
2025-02-24 17:25:15 -0500

    basic endpoints

--- backend/app.py
+++ backend/app.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 
+
+#list of endpoints
+
+#login - fetching users data/password combo / authenticate to the database
+
+#receive query from user and response in the same endpoint (through http obviously)
+
+#refresh
+
+#click individal buttons - fetch (resource related to that document with that buttons document ID
+
