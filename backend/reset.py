36c1431b69124eb6758e46570803b197f52135f3
Falsedeer <chen5292@purdue.edu>
2025-03-06 12:40:24 -0500

    Update reset.py with Database Table Setup

--- backend/reset.py
+++ backend/reset.py
@@ -1,12 +1,22 @@
+
 from tinydb import TinyDB, Query
 
 
 if __name__ == "__main__":
-    # open the connection and truncate all entry
+    # open the connection and truncate all entry in all tables
     db = TinyDB("database.json")
-    db.truncate()
+    for table in db.tables():
+        db.table(table).truncate()
+    
 
-    # insert the default dummy data
-    db.insert({"username": "chen5292", "email": "chen5292@purdue.edu", "password": "apple123"})
-    db.insert({"username": "chou610", "email": "chou610@purdue.edu", "password": "banana666"})
-    print(db.all())
+    # insert the dummy user entry in table `users`
+    users_table = db.table("users")
+    users_table.insert({"username": "chen5292", "email": "chen5292@purdue.edu", "password": "apple123"})
+    users_table.insert({"username": "chou610", "email": "chou610@purdue.edu", "password": "banana666"})
+    
+    # dump the whole database
+    print("[Table Dump]:\n{}\n".format(db.tables()))
+
+    # dump all the entry in all tables
+    for table in db.tables():
+        print("[Table] {}:\n{}\n".format(table, db.table(table).all()))
