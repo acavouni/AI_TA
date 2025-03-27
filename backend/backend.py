56a8303de23636512f267e56ddd0de6f335035ef
Falsedeer <chen5292@purdue.edu>
2025-03-27 10:36:34 -0400

    BackEnd Server Code Refinement

--- backend/backend.py
+++ backend/backend.py
@@ -1,24 +1,12 @@
 import os
 import uuid
+from typing import Any
 from flask import Flask
 from tinydb import TinyDB, Query
 
 
-class DatabaseAgent:
-	def __init__(self, database):
-		self.db = TinyDB(database)
 
-	def register_user(self, username: str, email: str, password: str) -> bool:
-		pass
 
-	def verify_user(self, username: str, password: str) -> bool:
-		pass
-
-	def remove_token(self, username: str, token: str) -> bool:
-		pass
-
-	def create_token(self, username: str) -> str:
-		pass
 
 
 class BackendServer:
@@ -27,6 +15,5 @@ class BackendServer:
 
 
 
-
 if __name__ == "__main__":
-	agent = DatabaseAgent("database.json")
+	pass
