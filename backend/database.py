9c6b2f4a9e61ee3ffa951e10f3476d6ec65c3488
Falsedeer <chen5292@purdue.edu>
2025-03-27 11:07:42 -0400

    Implemented Session Creation API

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 import uuid
 from typing import Any
 from tinydb import TinyDB, Query
+from datetime import datetime, timedelta, UTC
 
 
 class DatabaseAgent:
@@ -15,8 +16,7 @@ class DatabaseAgent:
 	def register_user(self, username: str, email: str, password: str) -> bool:
 		""" Create a new entry of user in the database """
 		doc_id = self.user.insert({"user_id": username, "email": email, "password": password})
-		if doc_id > 0: return True
-		else: return False
+		return True if doc_id else False
 
 
 	def verify_user(self, username: str, password: str) -> bool:
@@ -29,9 +29,21 @@ class DatabaseAgent:
 		else: return False
 
 
-	def verify_session(self, username: str, token: str) -> bool:
-		""" Verify a session token of the user """
-		pass
+	def create_session(self, username: str) -> str|None:
+		""" Create an UUID-V4 token for the session """
+		# verify whether the user exist before operation
+		if not self.users.contains(Query().user_id == username): return None
+
+		# token creation
+		session_token = str(uuid.uuid4())
+
+		# make the expire time 2 hours later after the token creation (UTC+0 Zone)
+		expiration_time = datetime.now(UTC) + timedelta(hours=2)
+		expire = expiration_time.timestamp()
+
+		# insert the entry to the database
+		doc_id = self.sessions.insert({"user_id": username, "token": session_token, "expires_at": expire})
+		return session_token if doc_id else None
 
 
 	def termiante_session(self, username: str, token: str) -> bool:
@@ -39,8 +51,8 @@ class DatabaseAgent:
 		pass
 
 
-	def create_session(self, username: str) -> str:
-		""" Create an UUID-V4 token for the session """
+	def verify_session(self, username: str, token: str) -> bool:
+		""" Verify a session token of the user """
 		pass
 
 
@@ -57,5 +69,5 @@ class DatabaseAgent:
 
 if __name__ == "__main__":
 	agent = DatabaseAgent("database.json")
-	status = agent.verify_user("chen5292", "apple123")
-	print(status)
+	token = agent.create_session("chen5292")
+	print("Token: ", token)
