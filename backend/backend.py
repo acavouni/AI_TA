1c6888876459ddeacdfeaf2e21bb232d9b1a727c
Falsedeer <chen5292@purdue.edu>
2025-04-04 06:28:35 -0400

    Preparing for Merging

--- backend/backend.py
+++ backend/backend.py
@@ -1,15 +1,15 @@
-import os
-import uuid
 from typing import Any
 from flask import Flask
-from tinydb import TinyDB, Query
-
+from database import DatabaseAgent
 
 
 class BackendServer:
 	def __init__(self):
 		pass
 
+
+def home():
+	return "Hello, World"
 
 
 if __name__ == "__main__":
